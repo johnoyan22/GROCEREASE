5fbbefd6e114cd9157e524ec77d6fd292da499cf
Vans Mcklyed Gonzales <vanzmckly@gmail.com>
2026-08-22 09:17:18 +0800

    Landing page, finalized

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -1,10 +1,12 @@
 import Navbar from "../components/NavBar";
 import HeroSection from "../components/HeroSection";
+import CategorySection from "../components/CategorySection";
 function LandingPage() {
     return (
         <div className="">
             <Navbar/>
             <HeroSection/>
+            <CategorySection/>
         </div>
     );
 }
